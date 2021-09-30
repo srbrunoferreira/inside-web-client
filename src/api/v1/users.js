0b9f2f9a0e3793544dbd8c3b03ad14f29ceaf731
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-09-30 16:42:00 -0300

    define project strucuture

---
+++ src/api/v1/users.js
@@ -0,0 +1,7 @@
+// Async functions that store the logic for making API requests.
+
+const getUsers = async () => {}
+
+const getUserFriends = async (userId) => {}
+
+export { getUsers, getUserFriends }
---
+++ src/components/Button/style.js
@@ -0,0 +1,1 @@
+// Use styled components here
