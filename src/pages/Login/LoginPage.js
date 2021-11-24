db3dba4db92ffa7a7bdae74a4cca26e7d306ed83
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-11-24 16:20:42 -0300

    feat: finish login screen

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField, Button, InputAdornment } from '@mui/material'
+import { TextField, Button, InputAdornment, Link } from '@mui/material'
 import { Person as PersonIcon, VpnKey as PasswordIcon } from '@mui/icons-material'
 
 import './LoginPage.css'
@@ -9,47 +9,67 @@ const LoginPage = () => {
   return (
     <div id="wrapper">
       <img id="logo" src={logoFile} />
-      <form id="login-form" style={{ padding: '50px' }}>
+      <form id="login-form">
         <div id="form-inputs-container">
-          <TextField
-            id="username"
-            label="Username"
-            type="text"
-            variant="filled"
-            size="small"
-            fullWidth
-            required
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <PersonIcon />
-                </InputAdornment>
-              ),
-              minLength: 8,
-              maxLength: 18
-            }}
-          />
-          <TextField
-            id="password"
-            label="Password"
-            type="password"
-            variant="filled"
-            size="small"
-            fullWidth
-            required
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <PasswordIcon />
-                </InputAdornment>
-              ),
-              minLength: 8,
-              maxLength: 18
-            }}
-          />
+          <div className="input-container">
+            <TextField
+              autoFocus
+              id="username"
+              label="Username"
+              type="text"
+              variant="filled"
+              size="small"
+              fullWidth
+              required
+              autoComplete='off'
+              focused
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+                minLength: 8,
+                maxLength: 18
+              }}
+            />
+          </div>
+          <div className="input-container">
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              variant="filled"
+              size="small"
+              fullWidth
+              required
+              autoComplete='off'
+              focused
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <PasswordIcon />
+                  </InputAdornment>
+                ),
+                minLength: 8,
+                maxLength: 18
+              }}
+            />
+          </div>
+        </div>
+        <div id="form-options-container">
+          <Link
+            type="button"
+            component="button"
+            variant="body2"
+            underline="none"
+            onClick={() => { }}
+          >
+            Esqueceu a senha?
+          </Link>
         </div>
         <div id="form-actions-container">
-          <Button id="form-btn-login" variant="contained" title="Login">Login</Button>
+          <Button id="form-btn-login" variant="contained" title="Login" type="submit" disableElevation>Login</Button>
           <Button id="form-btn-clear" variant="outlined" title="Login">Clear</Button>
         </div>
       </form>
